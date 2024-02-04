7da400a0c7d6e19dcac0771fc35125fd46752a77
tuanldVGU <cs2015_tuan.ld@student.vgu.edu.vn>
2024-02-04 16:48:17 +1030

    Update

--- src/components/Bell.jsx
+++ src/components/Bell.jsx
@@ -4,7 +4,6 @@ import classes from '../styles/bell.scss';
 const BellItem = ({ name, description, price }) => {
   return (
     <div className={classes.bellWrapper}>
-import classes from '../styles/bell.scss';
       <div className={classes.bell}>
         <div className="anchor material-icons layer-1">notifications_active</div>
         <div className="anchor material-icons layer-2">notifications</div>
